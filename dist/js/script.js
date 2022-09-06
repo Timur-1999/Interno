a48b804f030b1531bc080f6eacdf73731f71edde
Timur Asanov <t1m99asa@mail.ru>
2022-09-06 20:40:01 +0500

    did 50% adaptability

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
     $('.people__items').slick({
         slidesToShow: 3,
-        arrows: false,  autoplay: true,
+        arrows: false,
+         autoplay: true,
         autoplaySpeed: 5000,
+        responsive: [
+            {
+                breakpoint: 993,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 1,
+                    vertical: true,
+                }
+            }
+        ]
     });
   });
