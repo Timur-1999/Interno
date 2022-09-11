e659833227e3d81fea1d08c447354405343b552c
Timur Asanov <t1m99asa@mail.ru>
2022-09-11 17:06:25 +0500

    finished project

--- src/js/script.js
+++ src/js/script.js
@@ -20,4 +20,33 @@ $(document).ready(function(){
             }
         ]
     });
-  });
+
+    // Smooth scroll and pageup
+    $('.pageup').fadeOut("fast");
+    $(window).scroll(function () {
+    if($(this).scrollTop() > 1300){
+        $('.pageup').fadeIn();
+    } else{
+        $('.pageup').fadeOut();
+    }
+    });
+  
+    //Hamburger menu
+    window.addEventListener('DOMContentLoaded', () => {
+        const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu__link'),
+        hamburger = document.querySelector('.hamburger');
+
+        hamburger.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+
+        menuItem.forEach(item => {
+            item.addEventListener('click', () => {
+                hamburger.classList.toggle('hamburger_active');
+                menu.classList.toggle('menu_active');
+            })
+        })
+    });
+});
